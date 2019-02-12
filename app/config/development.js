48ef8efd8ac2dad52abffcb998bf22de38a498f9
Andrej Davidovič <andav1992@gmail.com>
2019-02-12 11:23:30 +0200

    Updated payment processor address

--- app/config/development.js
+++ app/config/development.js
@@ -4,7 +4,7 @@ export const PROVIDER_URL =
   'https://ropsten.infura.io/v3/1f09dda6cce44da68213cacb1ea9bb90';
 
 export const PAYMENT_PROCESSOR_ADDRESS =
-  '0x0948379e53a7f8df9dafcbb601bfc56faf8d8bd4';
+  '0xF9339B7106802FEd393E6c473E34B3963C7e3a92';
 
 export const PASSPORT_FACTORY_ADDRESS =
   '0x438bbf17582346f2bb4c47da0fd33231f688e805';
